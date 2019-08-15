bea7c27c28448676a0b2d4d90791415fe9b8f4b9
Trần Trọng Tuệ <tttue@yahoo.com>
2019-08-15 17:52:28 +0900

    finish

--- server.js
+++ server.js
@@ -1,32 +1,54 @@
 // server.js
 // where your node app starts
+/*
+	npm install express cors
+*/
 
 // init project
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+	res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+	res.json({ greeting: 'hello API' });
 });
 
+// Main
+app.get("/api/timestamp/:date_string?", function (req, res) {
+	var date = new Date();
+	if (req.params.date_string) {
+		var date_string = req.params.date_string;
+		if (date_string[4]=="-" && date_string[7]=="-"){
+		} else {
+			date_string = parseInt(date_string)*1000;
+		}
+		date = new Date(date_string);
+	}
+	if (typeof date==="string"){
+		res.json({ error: date });
+	}else {
+		res.json({ "unix": date.getTime(), "utc": date.toUTCString() });
+	}
+
+
+});
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT|3000, function () {
+	console.log('Your app is listening on port ' + listener.address().port);
 });
