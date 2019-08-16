f77226f0e5f3e9c4176190fa965a6c5653637a16
Trần Trọng Tuệ <tttue@yahoo.com>
2019-08-16 11:32:45 +0900

    Change  to ES6 module export

--- tools.js
+++ tools.js
@@ -1,15 +1,30 @@
-module.exports = {
-	refineDateString: function(date_s) {
-		function refine(data) {
-			data = data.replace(/^0*/, "");
-			if (data.length < 2) {
-				data = "0" + data;
-			}
-			return data;
-		}
-		var dateA = date_s.split("-")
-		var year = dateA[0], month = dateA[1], day = dateA[2];
+// module.exports = {
+// 	refineDateString: function(date_s) {
+// 		function refine(data) {
+// 			data = data.replace(/^0*/, "");
+// 			if (data.length < 2) {
+// 				data = "0" + data;
+// 			}
+// 			return data;
+// 		}
+// 		var dateA = date_s.split("-")
+// 		var year = dateA[0], month = dateA[1], day = dateA[2];
 
-		return year + "-" + refine(month) + "-" + refine(day);
+// 		return year + "-" + refine(month) + "-" + refine(day);
+// 	}
+// }
+
+function refineDateString(date_s) {
+	function refine(data) {
+		data = data.replace(/^0*/, "");
+		if (data.length < 2) {
+			data = "0" + data;
+		}
+		return data;
 	}
+	var dateA = date_s.split("-")
+	var year = dateA[0], month = dateA[1], day = dateA[2];
+
+	return year + "-" + refine(month) + "-" + refine(day);
 }
+module.exports.refineDateString = refineDateString;
